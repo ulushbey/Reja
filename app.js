c49371bad5e4cf997cd07714ac3bc67d2b777a02
ulushbey <ulushbey.h@gmail.com>
2024-02-14 11:57:07 +0500

    feat: Reja mini loyihani frontendini qurish

--- app.js
+++ app.js
@@ -32,14 +32,8 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log('user entered /create-item');
   const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja}, (err,data) =>{
-  if(err){
-    console.log(err);
-    res.end("something went wrong");
-
-  }else{
-    res.end("successfully added");
-  }
+  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+  res.json(data.ops[0]);
 });
 });
 
